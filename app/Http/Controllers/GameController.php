b3c96340f82aacb8cfee7af8925c4f000436adc9
JoshuaFaraday <andrejminar66@gmail.com>
2022-01-08 23:24:47 +0100

    features: games crud + new table na prvu stranku

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Game;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+
+    public function adminIndex() {
+        return view('admin.games',['games'=>Game::all()]);
+    }
+
+
+
+    public function edit(Game $game)
+    {
+        return view('admin.games.edit', ['game' => $game]);
+    }
+
+
+    public function update(Request $request, Game $game)
+    {
+        $attributes = $request->validate([
+            'image' => 'required|image',
+            'name' => 'required|string',
+            'relase_date' => 'required|digits:4|integer|min:1900|max:2099',
+            'platform' => 'required|string',
+            'genre' => 'required|string',
+            'HW_requirements' => 'required|string',
+            'rating' => 'required|integer|min:0|max:100',
+            'description' => 'required|string'
+        ]);
+        $path = request()->file('image')->store('games', 'public');
+        $attributes['image'] = $path;
+        $game->update($attributes);
+        return redirect()->route('admin.games');
+    }
+
+    public function destroy(Game $game)
+    {
+        $game->delete();
+        return redirect()->route('admin.games');
+    }
+
+    public function create()
+    {
+        return view('admin.games.create');
+
+    }
+
+    public function store()
+    {
+
+        $attributes = request()->validate([
+            'image' => 'required|image',
+            'name' => 'required|string',
+            'relase_date' => 'required|digits:4|integer|min:1900|max:2099',
+            'platform' => 'required|string',
+            'genre' => 'required|string',
+            'HW_requirements' => 'required|string',
+            'rating' => 'required|integer|min:0|max:100',
+            'description' => 'required|string'
+
+        ]);
+        $path = request()->file('image')->store('games', 'public');
+        $attributes['image'] = $path;
+
+
+        Game::create($attributes);
+        return redirect()->route('admin.games');
+    }
+
+
+}
