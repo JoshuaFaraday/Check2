66ac7eab9ea346d860e7a8e6ff41d6b08acc3ad1
JoshuaFaraday <andrejminar66@gmail.com>
2022-01-17 18:51:52 +0100

    refactoring, fix userController

---
+++ app/Http/Requests/Users/CreateUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Users;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        /*Kontrola či je admin pri validacii*/
+        return Auth::id() === 1;
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,',
+            'password' => 'required|min:8|confirmed'
+        ];
+    }
+}
